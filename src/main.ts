03921ea09cb0f5548b7367eda82e959e7387c2b4
Axehco <mbozhong@outlook.com>
2022-12-02 15:58:08 +0800

    3-5通过图片名动态加载图片代码实现-3

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { LmgLoader } from './utils/imgUtil'
+
+LmgLoader.storageAllImg()
 
 createApp(App).mount('#app')
+
+// console.log(import.meta.env)
+// console.log(import.meta.env.VITE_username)
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,50 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { CommonServerOptions, defineConfig } from 'vite'
+import fs from 'fs'
+import dotenv, { DotenvParseOutput } from 'dotenv'
 
-// https://vitejs.dev/config/
+/* // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()]
+}) */
+
+// 重新用函数方式定义： defineConfig
+export default defineConfig((mode) => {
+  // console.log('当前在什么环境运行项目:', mode.mode)
+
+  //  拼接当前环境文件名
+  const envFileName: string = '.env'
+  const curEnvFileName = `${envFileName}.${mode.mode}`
+  // console.log('curEnvFileName:', curEnvFileName)
+
+  // let server: CommonServerOptions = {}
+  let server: Record<string, any> = {}
+  const envData = fs.readFileSync(curEnvFileName)
+  const envMap: DotenvParseOutput = dotenv.parse(envData)
+  // console.log(envMap)
+  // console.log('@: ', envMap.VITE_PORT)
+  // console.log(typeof(envMap.VITE_PORT))
+
+  if (mode.mode === "development") {
+    server = {
+      host: envMap.VITE_HOST,
+      port: envMap.VITE_PORT,
+      proxy: {
+        [envMap.VITE_BASE_URL]: {
+          target: envMap.VITE_PROXY_DOMAIN,
+        },
+      }
+    }
+    // console.log('我是生产者环境', server)
+  } else {
+    // console.log('我是生产者环境')
+    server = {
+      port: envMap.VITE_PORT,
+      host: envMap.VITE_HOST,
+    }
+  }
+
+  return {
+    plugins: [vue()]
+  }
 })
